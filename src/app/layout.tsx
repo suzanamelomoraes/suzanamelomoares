077e20c2f6bdeca96522659266f50f2e4de1f4fc
Suzana Melo <suzanamelojor@gmail.com>
2023-06-07 22:31:10 +1200

    import and apply header on the top of all children

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Poppins } from "next/font/google";
+import Header from "./components/Header";
 
 const poppins = Poppins({ weight: ["400", "700"], subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
+        <Header />
         <main className="container">{children}</main>
       </body>
     </html>
