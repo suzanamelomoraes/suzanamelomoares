12c22b09531470efb14514e32958e9a5bfe93e83
Suzana Melo <suzanamelojor@gmail.com>
2023-06-12 18:19:21 +1200

    rename about to community

---
+++ src/app/community/page.tsx
@@ -0,0 +1,18 @@
+export const metadata = {
+  title: "Tech Community",
+};
+
+const CommunityPage = () => {
+  return (
+    <div>
+      <h1>Tech Community</h1>
+      <p>
+        Meetups I help to organise in New Zealand <br />
+        <br />
+        Tech community in Auckland
+      </p>
+    </div>
+  );
+};
+
+export default CommunityPage;
