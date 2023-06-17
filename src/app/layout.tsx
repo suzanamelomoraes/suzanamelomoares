9c7db0f2b1efb844b7588da326341fe7e44454fa
Suzana Melo <suzanamelojor@gmail.com>
2023-06-17 16:11:36 +1200

    include property to fix issues about extension passing these extra attributes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>
+      <body className={poppins.className} suppressHydrationWarning={true}>
         <Header />
         <main className="container">{children}</main>
       </body>
